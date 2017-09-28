23c9f71bae538a4a6fd84bed2887e1fb3f663baf
Michelle <michelleamcsweeney@gmail.com>
2017-09-28 15:27:13 -0400

    Update getKMLs.py

--- Data/04_Webscraping/getKMLs.py
+++ Data/04_Webscraping/getKMLs.py
@@ -6,13 +6,9 @@ Created on Tue Sep 26 22:21:31 2017
 @author: mam
 """
 
-import requests, bs4
 import urllib.request
 
-
 url="https://census2011.adrianfrith.com"
-#page = requests.get(url+"/place/798")
-#apage = bs4.BeautifulSoup(page.text)
 
 from listfile import mylist as mylist
 
